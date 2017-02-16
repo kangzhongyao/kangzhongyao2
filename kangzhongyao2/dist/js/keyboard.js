a84b15aed30e2e46809046ad804cab84178d75ce
kangzhongyao <983606046@qq.com>
2017-02-16 15:09:41 +0800

    用bootstrap打造的后端管理系统

---
+++ kangzhongyao2/dist/js/keyboard.js
@@ -0,0 +1,79 @@
+$(function(){
+	var $write = $('.write'),
+		shift = false,
+		capslock = false;
+	// Download by http://www.codefans.net
+	$('#keyboard li').click(function(){
+		var $this = $(this),
+			character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
+		
+		// Shift keys
+		if ($this.hasClass('left-shift') || $this.hasClass('right-shift')) {
+			$('.letter').toggleClass('uppercase');
+			$('.symbol span').toggle();
+			
+			shift = (shift === true) ? false : true;
+			capslock = false;
+			return false;
+		}
+		
+		// Caps lock
+		if ($this.hasClass('capslock')) {
+			$('.letter').toggleClass('uppercase');
+			capslock = true;
+			return false;
+		}
+		
+		// Delete
+		if ($this.hasClass('delete')) {
+			var html = $('.write').val();
+			
+			$('.write').val(html.substr(0, html.length - 1));
+			return false;
+		}
+		
+		// Special characters
+		if ($this.hasClass('symbol')) character = $('span:visible', $this).html();
+		if ($this.hasClass('space')) character = ' ';
+		if ($this.hasClass('tab')) character = "\t";
+		if ($this.hasClass('return')) character = "\n";
+		
+		// Uppercase letter
+		if ($this.hasClass('uppercase')) character = character.toUpperCase();
+		
+		// Remove shift once a key is clicked.
+		if (shift === true) {
+			$('.symbol span').toggle();
+			if (capslock === false) $('.letter').toggleClass('uppercase');
+			
+			shift = false;
+		}
+		console.log(character);
+		// Add the character
+		$('.write').val($('.write').val()+character);
+	});
+});
+
+/**
+ *   ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　＞　　　＜　┃
+ * ┃　　　　　　　┃
+ * ┃...　⌒　...　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *     ┃　　　┃　
+ *     ┃　　　┃
+ *     ┃　　　┃
+ *     ┃　　　┃  神兽保佑
+ *     ┃　　　┃  代码无bug　　
+ *     ┃　　　┃
+ *     ┃　　　┗━━━┓
+ *     ┃　　　　　　　┣┓
+ *     ┃　　　　　　　┏┛
+ *     ┗┓┓┏━┳┓┏┛
+ *       ┃┫┫　┃┫┫
+ *       ┗┻┛　┗┻┛
+ */
